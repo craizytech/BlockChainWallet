e26ca1713cca5d872b2bb751cdea4980d2a83536
craizytech <craizytech@gmail.com>
2024-07-24 22:21:17 +0300

    restructured the project to remove any introduction of orms

--- app/config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://your_db_user:your_db_password@localhost/system_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'your_jwt_secret_key'
