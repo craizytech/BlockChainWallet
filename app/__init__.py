7c65787b4b466cb9afaafffc3b0bcff7a1ad39e7
craizytech <craizytech@gmail.com>
2024-07-30 20:36:00 +0300

    initialized cors functionality

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from app.config import Config
-from app.extensions import jwt
+from app.extensions import jwt, cors
 from app.auth import auth_bp
 from app.dashboard import dashboard_bp
 from app.monitoring import monitoring_bp
@@ -11,16 +11,10 @@ from app.transactions import transactions_bp
 def create_app():
     app = Flask(__name__)
 
-    # Enable CORS with detailed settings
-    CORS(app, supports_credentials=True, resources={
-        r"/*": {
-            "origins": "*",
-            "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-            "allow_headers": ["Content-Type", "Authorization"]
-        }
-    })
-
     app.config.from_object(Config)
+
+    # Enable CORS with detailed settings
+    cors.init_app(app)
 
     # Initialize JWT
     jwt.init_app(app)
